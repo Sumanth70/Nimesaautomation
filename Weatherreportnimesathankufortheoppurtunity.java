e44b8e874a2bd5ca8995c790473c08864bc7562d
Sumanth70 <49972273+Sumanth70@users.noreply.github.com>
2020-05-31 17:44:56 +0530

    Add files via upload

---
+++ Weatherreportnimesathankufortheoppurtunity.java
@@ -0,0 +1,133 @@
+
+	package Nimesa;
+
+	import java.io.File;
+	import java.io.FileNotFoundException;
+	import java.util.Scanner;
+
+	public class Weatherreportnimesathankufortheoppurtunity 
+	{
+		String nextWord;
+	    String url = "https://samples.openweathermap.org/data/2.5/forecast/hourly?q=London,us&appid=b6907d289e10d714a6e88b30761fae22";
+
+		private void input(File f) throws FileNotFoundException {
+		        Scanner input = new Scanner(f);     
+		while (input.hasNext()) {
+	           nextWord = input.next();
+		if(nextWord.contains ("temp"));
+		if(nextWord.contains ("id"));
+		if(nextWord.contains ("speed"));
+		if(nextWord.contains ("2019-03-27"));	
+		System.out.println("Day1 exits");
+
+	   {
+		if(nextWord.contains ("temp"));
+		if(nextWord.contains ("id"));
+		if(nextWord.contains ("speed"));
+		if(nextWord.contains ("2019-03-28"));	
+		System.out.println("Day2 exits");
+	   }
+	   {
+		if (nextWord.contains ("temp"));
+		if (nextWord.contains ("id"));
+		if (nextWord.contains ("speed"));
+		if(nextWord.contains ("2019-03-29"));	
+		System.out.println("Day3 exits");
+	   }
+	   {
+		if (nextWord.contains ("temp"));
+		if (nextWord.contains ("id"));
+		if (nextWord.contains ("speed"));
+		if(nextWord.contains ("2019-03-30"));	
+	    System.out.println("Day4 exits");
+	   }
+		}
+		}
+		private int id;
+		private String main;
+		private String description;
+		private String icon ;
+		private String forecast;
+		public boolean hasid() {
+		
+		int hour = time.getHoursdata();
+			return this.id != Integer.BYTES;
+		}
+		public boolean hasmain() {
+			int hour = time.getHoursdata();
+			return this.main != null;
+		}
+		public boolean hasdescription() {
+			int hour = time.getHoursdata();
+			return this.description != null;
+		}
+		public boolean icon() {
+			int hour = time.getHoursdata();
+			return this.icon != null;
+		}
+	   public boolean hasForecast () {
+			return this.forecast != null && !this.forecast.isEmpty ();
+		}
+	   
+	    String temp_min;
+	    String temp_max;
+
+	    while (input.hasNext()) {
+	    nextWord = input.next();
+
+	    String Day1;
+		if (nextWord.contains(Day1)) {
+
+	    if (nextWord.contains("temp")) {
+	    String temp = nextWord;
+	    temp.compareTo("temp_min");
+		temp.replace(temp, temp_min);
+	                
+	    temp.compareTo("temp_max");
+	    temp.replace(temp, temp_max);
+	                
+	                   
+	    String Day2;
+	    if (nextWord.contains(Day2)) {
+
+	    if (nextWord.contains("temp")) {
+	    temp.compareTo("temp_min");                         
+	    temp.replace(temp, temp_min);
+	                            
+	    temp.compareTo("temp_max");
+	    temp.replace(temp, temp_max);
+	                            
+	    String Day3;
+	    if (nextWord.contains(Day3)) {
+
+	    if (nextWord.contains("temp")) {
+	    temp.compareTo("temp_min");
+	                
+	    temp.replace(temp, temp_min);
+	    temp.compareTo("temp_max");
+	    temp.replace(temp, temp_max);
+	                
+	    String Day4;
+	    if (nextWord.contains(Day4)) {
+	    if (nextWord.contains("temp")) {
+	                
+	    temp.compareTo("temp_min");
+	    temp.replace(temp, temp_min);
+	    temp.compareTo("temp_max");
+	    temp.replace(temp, temp_max);
+	           private String conditionManager(int weather_id) {
+	                    String answer = "";
+	                    if (weather_id == 500)
+	                        answer += "light rain";
+
+	                    if (weather_id == 800)
+	                        answer += "clear sky";
+
+	          
+
+	                    return answer;
+	                }
+	           
+	    }
+	    }
+	    
